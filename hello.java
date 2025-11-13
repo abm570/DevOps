67a324c1849c0d9e7f784a3eeb27a86566ce5460
abm570 <134905856+abm570@users.noreply.github.com>
2025-11-13 11:41:06 +0530

    Update and rename demo.java to hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class demo
 {
   public static void main(String args[])
   {
-    System.out.println("Running Java File from Jenkins Pipeline");
+    System.out.println("Demo on Running Java File from Jenkins Pipeline");
   }    
 }
